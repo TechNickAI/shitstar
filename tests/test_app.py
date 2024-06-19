b3518b189a0c67b9ae0de4150ddd9014c7eb7448
Nick Sullivan <nick@sullivanflock.com>
2024-06-19 10:26:29 +0200

    Basic test for streamlit execution

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,2 +1,12 @@
-def test_app():
-    assert True
+from streamlit.testing.v1 import AppTest
+
+
+def test_streamlit_app_runs():
+    # Initialize the app
+    at = AppTest.from_file("app.py")
+
+    # Run the app
+    at.run(timeout=10_000)
+
+    # Check if the app ran without exceptions
+    assert not at.exception, "Streamlit app encountered an exception"
